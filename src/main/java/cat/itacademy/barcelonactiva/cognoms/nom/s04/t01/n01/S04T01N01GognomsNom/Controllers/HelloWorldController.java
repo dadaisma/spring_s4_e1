363e683117e8a55f927a211f388ce0c4b0166c65
dadaisma <infodadaisma@gmail.com>
2024-06-06 21:45:29 +0200

    responsebodyannotationadded

--- src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n01/S04T01N01GognomsNom/Controllers/HelloWorldController.java
+++ src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n01/S04T01N01GognomsNom/Controllers/HelloWorldController.java
@@ -1,24 +1,23 @@
 package cat.itacademy.barcelonactiva.cognoms.nom.s04.t01.n01.S04T01N01GognomsNom.Controllers;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloWorldController {
 
     @GetMapping("/HelloWorld")
-    public String saluda(@RequestParam(defaultValue = "UNKNOWN") String nombre) {
-        return "Hola, " + nombre + ". Estás ejecutando un proyecto Maven.";
+    @ResponseBody
+    public String saluda(@RequestParam(defaultValue = "UNKNOWN")  String name) {
+        return "Hola, " + name + ". Estás ejecutando un proyecto Maven.";
     }
 
-    @GetMapping({"/HelloWorld2", "/HelloWorld2/{nombre}"})
-    public String saluda2(@PathVariable(required = false) String nombre) {
-        if (nombre == null) {
-            nombre = "";
+    @GetMapping({"/HelloWorld2", "/HelloWorld2/{name}"})
+    @ResponseBody
+    public String saluda2(@PathVariable(required = false) String name) {
+        if (name == null) {
+            name = "";
         }
-        return "Hola, " + nombre + ". Estás ejecutando un proyecto Maven.";
+        return "Hola, " + name + ". Estás ejecutando un proyecto Maven.";
     }
 }
